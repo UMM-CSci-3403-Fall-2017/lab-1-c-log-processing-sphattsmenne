ac587ec6c85cb3dae923cd825df0d795844b3a82
Charles Menne <menne019@bebop.morris.umn.edu>
2017-09-07 13:51:19 -0500

    Created scratch directory for testing purposes
    Modified the location of process_client.logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
 logDir=$1
+location=$(pwd)/*
 
-cd ..
+echo $location
+cd $logDir/*/var/log
 
-cd $logDir
 
-cat $logDir/* | awk '/Failed password for/ {print $1,$2,$3,$9,$11,$13;}' | awk '/invalid/ {print $1,$2,substr($3, 1, 2),$5,$6;} ! /invalid/ {print $1,$2,substr($3, 1, 2),$4,$5;}' > failed_login_data.txt
+cat * | awk '/Failed password for/ {print $1,$2,$3,$9,$11,$13;}' | awk '/invalid/ {print $1,$2,substr($3, 1, 2),$5,$6;} ! /invalid/ {print $1,$2,substr($3, 1, 2),$4,$5;}' > failed_login_data.txt
+
+#mv failed_login_data.txt $location
+
+
 
 
