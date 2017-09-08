c5d87c5d56d5949866620e01cdab65226d6514b8
Charles Menne <menne019@axiom.morris.umn.edu>
2017-09-07 23:37:19 -0500

    Created create_username_dist.sh. Tests all pass
    Moved wrap_contents.sh into bin to be used in create_username_dist.sh

---
+++ bin/create_username_dist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Grabs the directory to work in
+Dir=$1
+
+# Searches through the given directory for every failed_login_data.txt. Then we use awk to grab the name in column 4 and put it in holder.txt to be used later
+for i in $Dir/*
+do
+    awk '{print $4}' $i/failed_login_data.txt >> holder.txt
+done
+
+# Used sort to sort holder.txt so uniq can get the number of occurences of each name. Then we used awk to format the data into html. Finally we use cat to place that in a new txt file called holder2.txt
+sort holder.txt | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat > holder2.txt
+
+# Used our wrap_contents script in bin to wrap our holder2.txt with the html header and footer needed
+./bin/wrap_contents.sh holder2.txt ./html_components/username_dist $Dir/username_dist.html
+
+# Removes the two temporary holder files we used
+rm holder.txt
+rm holder2.txt
